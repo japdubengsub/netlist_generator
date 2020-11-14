1126d49c729eba96c6212ac0a7d22c3f1e64656a
Alexander Barannikov <japdubengsub@gmail.com>
2020-11-14 22:49:39 +0300

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ use ipnet::Ipv4Net;
 use std::cmp::{max, min};
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
-
 pub trait NetSize {
     fn size(&self) -> u32;
 }
@@ -38,8 +37,8 @@ pub trait Resize {
 
 impl Resize for Vec<Ipv4Net> {
     fn resize_with_prefix(&mut self, new_prefix: u8) -> Vec<Ipv4Net> {
-//        println!("Records:   {:#?}", self.len());
-//        println!("Addresses: {:#?}", self.size());
+        // println!("Records:   {:#?}", self.len());
+        // println!("Addresses: {:#?}", self.size());
 
         let mut net_buff: Vec<Ipv4Net> = Vec::with_capacity(2);
         net_buff.push("10.10.0.0/32".parse().unwrap());
@@ -47,14 +46,14 @@ impl Resize for Vec<Ipv4Net> {
 
         // amount of iterations
         let mut range = max(self.len(), 1) - 1;
-        //println!("range {:#?}", range);
+        // println!("range {:#?}", range);
 
         for index in 0..range {
-//            print_sep();
-//            println!("INDEX {:#?}", index);
-//            for (i, net) in self.iter().enumerate() {
-//                println!("{:#?} -> {:#?} -> {:#?}", i, net, net.size());
-//            }
+            // print_sep();
+            // println!("INDEX {:#?}", index);
+            // for (i, net) in self.iter().enumerate() {
+            //     println!("{:#?} -> {:#?} -> {:#?}", i, net, net.size());
+            // }
 
             let mut current_net = self[index];
             let mut next_net = self[index + 1];
@@ -62,38 +61,37 @@ impl Resize for Vec<Ipv4Net> {
             let ip = current_net.network();
             let ip_next = next_net.network();
 
-//            println!("");
-//            println!(
-//                "{:#?}  :  {:#?} -> {:#?}",
-//                current_net,
-//                current_net.network(),
-//                current_net.broadcast()
-//            );
-//            println!(
-//                "{:#?}  :  {:#?} -> {:#?}",
-//                next_net,
-//                next_net.network(),
-//                next_net.broadcast()
-//            );
-//            println!("                {:#?} -> {:#?}", ip, ip_next);
+            // println!("");
+            // println!(
+            //     "{:#?}  :  {:#?} -> {:#?}",
+            //     current_net,
+            //     current_net.network(),
+            //     current_net.broadcast()
+            // );
+            // println!(
+            //     "{:#?}  :  {:#?} -> {:#?}",
+            //     next_net,
+            //     next_net.network(),
+            //     next_net.broadcast()
+            // );
+            // println!("                {:#?} -> {:#?}", ip, ip_next);
 
             //
             // RESIZE
             //
-//            print_sep();
             let max_prefix = max(current_net.prefix_len(), next_net.prefix_len());
-//            println!("max_prefix = {:#?}", max_prefix);
-//            println!("new_prefix = {:#?}", new_prefix);
-
-//            if max_prefix < new_prefix {
-//                dbg!(current_net, next_net);
-//                dbg!(max_prefix, new_prefix);
-//                //dbg!(max_prefix - new_prefix + 1);
-//            }
-            let n: u8 = max(max_prefix as i8 - new_prefix as i8 + 1, 0) as u8;  // todo remove cast
+            // println!("max_prefix = {:#?}", max_prefix);
+            // println!("new_prefix = {:#?}", new_prefix);
+            //
+            // if max_prefix < new_prefix {
+            //     dbg!(current_net, next_net);
+            //     dbg!(max_prefix, new_prefix);
+            //     //dbg!(max_prefix - new_prefix + 1);
+            // }
+            let n: u8 = max(max_prefix as i8 - new_prefix as i8 + 1, 0) as u8; // todo remove cast
             for i in 0..n {
                 let current_prefix = max_prefix - i;
-//                println!("current_prefix = {}", current_prefix);
+                // println!("current_prefix = {}", current_prefix);
 
                 if current_net.prefix_len() > current_prefix {
                     current_net = Ipv4Net::new(ip, current_prefix).unwrap();
@@ -104,9 +102,9 @@ impl Resize for Vec<Ipv4Net> {
 
                 net_buff[0] = current_net;
                 net_buff[1] = next_net;
-//                println!("net_buff = {:#?}", net_buff);
+                // println!("net_buff = {:#?}", net_buff);
                 let net_buff2 = Ipv4Net::aggregate(&net_buff);
-//                println!("net_buff2 = {:#?}", net_buff2);
+                // println!("net_buff2 = {:#?}", net_buff2);
 
                 if net_buff2.len() == 1 {
                     self[index] = current_net;
@@ -116,36 +114,12 @@ impl Resize for Vec<Ipv4Net> {
             }
         }
 
-//        print_sep();
-//        print_sep();
-//        println!("after resize");
-////        for net in self.iter() {
-////            println!("{:#?} -> {:#?}", net, net.size());
-////        }
-//        println!("Records:   {:#?}", self.len());
-//        println!("Addresses: {:#?}", self.size());
-
-//        print_sep();
-//        print_sep();
-
-//        println!("after resize and aggregation");
         let mut out = Ipv4Net::aggregate(&self);
-//        for net in out.iter() {
-//            println!("{:#?} -> {:#?}", net, net.size());
-//        }
-//        println!("Records:   {:#?}", out.len());
-//        println!("Addresses: {:#?}", out.size());
-
-//        println!("after resize and aggregation v2");
-//        out.sort();
-//        let mut out = Ipv4Net::aggregate(&out);
-//        println!("Records:   {:#?}", out.len());
-//        println!("Addresses: {:#?}", out.size());
 
         return out;
     }
 }
 
-fn print_sep() {
+pub fn print_sep() {
     println!("{}", "=".repeat(120));
 }
--- src/main.rs
+++ src/main.rs
@@ -5,94 +5,76 @@
 #![allow(unreachable_code)]
 
 use std::any::type_name;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::{stdin, BufRead, BufReader, Read, Write};
 use std::mem;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use std::process::exit;
 use std::slice::Split;
 use std::time::{Duration, Instant};
-use std::path::Path;
-use std::error::Error;
-use std::io::LineWriter;
-use std::fs::OpenOptions;
-
 
 use encoding_rs::mem::ensure_utf16_validity;
 use encoding_rs::WINDOWS_1251;
 use encoding_rs_io::{DecodeReaderBytes, DecodeReaderBytesBuilder};
 use ipnet::{IpNet, Ipv4Net, Ipv6Net};
 
+use netlist_generator::print_sep;
 use netlist_generator::NetSize;
 use netlist_generator::Resize;
+use std::fmt::format;
+use std::iter::Inspect;
+use std::path::Path;
 
-//const CSV_FILE: &str = "A:\\dump2.csv";
-
-//const CSV_FILE: &str = "dump.csv";
-//const OUT_FILE: &str = "norm_list.txt";
-
-const CSV_FILE: &str = "/root/z-i/dump.csv";
-const OUT_FILE: &str = "/etc/bird/norm_list.txt";
-
-const ROUTES_MAX: usize = 300_000;
-const ROUTE_TEMPLATE: &str = "route {} reject;";
-
-
-fn print_sep() {
-    println!("{}", "=".repeat(80));
-}
+const CSV_FILE: &str = "A:\\dump2.csv";
+const OUT_FILE: &str = "A:\\dump2.txt";
+const PREFIX: u8 = 24;
+const MAX_ROUTES: u32 = 300_000;
 
 fn type_of<T>(_: T) -> &'static str {
     type_name::<T>()
 }
 
+// struct for statistics of the file
 #[derive(Debug, Default)]
 struct Stat {
     total_file_lines: u32,
     ipv4_hosts: u32,
     ipv4_networks: u32,
     ipv6_hosts: u32,
-    ipv6_networks: u32,
+    ipv6_networks: u32, // isn't supported
 }
 
 fn read_file(file: File) -> Vec<Ipv4Net> {
     //    color_backtrace::install();
-    let mut input = String::new();
-    //    println!("hello {}", input);
-    //    stdin().read_line(&mut input).expect("Couldn't read line");
+    println!("reading file...");
 
     let mut stat: Stat = Default::default();
-//    let mut stat = Stat {
-//        total_file_lines: 0,
-//        ipv4_hosts: 0,
-//        ipv4_networks: 0,
-//        ipv6_hosts: 0,
-//        ipv6_networks: 0,
-//    };
+    let file_size = file.metadata().unwrap().len();
 
     let transcoded = DecodeReaderBytesBuilder::new()
         .encoding(Some(WINDOWS_1251))
         .build(file);
     let file_reader = BufReader::new(transcoded);
 
-//    let mut net_list: Vec<Ipv4Net> = Vec::with_capacity(3_000_000);
+    // let mut net_list: Vec<Ipv4Net> = Vec::with_capacity(3_000_000);
     let mut net_list: Vec<Ipv4Net> = Vec::new();
-    //    println!("hello {}", input);
-    //    stdin().read_line(&mut input).expect("Couldn't read line");
 
-    for line in file_reader.lines() {
-        //        println!("{}", LINE_SEP);
+    // todo
+    // will use 1 cache/previous value as input filter
+    // let mut prev_line: &str = "";
+    // let mut prev_line: String: = "";
+
+    for file_line in file_reader.lines() {
         stat.total_file_lines += 1;
-        let file_line = line.unwrap();
-        //        println!("{}   :::   {:?}", num, l);
+        let file_line = file_line.unwrap();
 
-        for mut substring in file_line.split(" | ") {
-            //            println!("{}", "=".repeat(20));
-            //            println!("{:#?}", substring);
-            //            println!("{:#?}", type_of(substring));
+        for substring in file_line.split(" | ") {
+            // println!("{}", "=".repeat(20));
+            // println!("{:#?}", substring);
+            // println!("{:#?}", type_of(substring));
 
             let substring: &str = substring.split(';').next().unwrap();
-            //            println!("{:#?}", substring);
+            // println!("{:#?}", substring);
 
             let result = check_addr(substring);
 
@@ -117,49 +99,53 @@ fn read_file(file: File) -> Vec<Ipv4Net> {
             };
         }
 
-        //        if total_file_lines_cnt == 2 {
-        //            break;
-        //        }
+        // prev_line = &file_line;
+        // println!("hello {}", &prev_line);
+
+        // fixme remove
+        // if stat.total_file_lines >= 5 {
+        //     exit(101);
+        //     break;
+        // }
     }
 
-    //    println!("hello {}", input);
-    //    stdin().read_line(&mut input).expect("Couldn't read line");
-
+    println!("reading finished...");
     print_sep();
-    println!("{:#?}", stat);
+    println!(
+        "IPv4 Hosts:          {:>12}",
+        decimal_mark(stat.ipv4_hosts.to_string())
+    );
+    println!(
+        "IPv4 Nets:           {:>12}",
+        decimal_mark(stat.ipv4_networks.to_string())
+    );
+    println!(
+        "IPv6 Hosts:          {:>12}",
+        decimal_mark(stat.ipv6_hosts.to_string())
+    );
+    println!(
+        "IPv6 Nets:           {:>12}",
+        decimal_mark(stat.ipv6_networks.to_string())
+    );
+    println!(
+        "Total file lines:    {:>12}",
+        decimal_mark(stat.total_file_lines.to_string())
+    );
+    println!(
+        "File size:           {:>12} bytes",
+        decimal_mark(file_size.to_string())
+    );
+    print_sep();
 
     net_list
 }
 
-fn write_file(ip_list: Vec<Ipv4Net>) {
-    // Create a path to the desired file
-    let path = Path::new(OUT_FILE);
-
-    let mut file = OpenOptions::new()
-        .read(false)
-        .write(true)
-        .create(true)
-        .append(false)
-        .open(&path).unwrap();
-
-    for net in ip_list {
-        let line = format!("route {} reject;\n", net);
-        file.write_all(line.as_bytes());
-    }
-}
-
 fn check_addr(addr_string: &str) -> AddressType {
-    let mut digits_counter = 0;
-    let mut letters_counter = 0;
-    let mut dots_counter = 0;
-    let mut slashes_counter = 0;
-    let mut colons_counter = 0;
-
-    //    if addr_string.len() > 40 {
-    //        println!("{}", "MORE THAN 40 !!!");
-    //        println!("{}", addr_string);
-    //        return AddressType::None;
-    //    }
+    let mut digits_counter: u32 = 0;
+    let mut letters_counter: u32 = 0;
+    let mut dots_counter: u32 = 0;
+    let mut slashes_counter: u32 = 0;
+    let mut colons_counter: u32 = 0;
 
     for c in addr_string.chars() {
         if c.is_ascii_digit() {
@@ -187,10 +173,10 @@ fn check_addr(addr_string: &str) -> AddressType {
     {
         return AddressType::IPv4;
     // ipv6
-    } else if colons_counter > 0
+    } else if colons_counter == 7
         && slashes_counter == 0
         && dots_counter == 0
-        && (digits_counter + letters_counter) > 0
+        && (digits_counter + letters_counter) == 32
     {
         return AddressType::IPv6;
     // ipv4 subnet
@@ -215,8 +201,10 @@ enum AddressType {
 
 fn main() {
     //    color_backtrace::install();
-    let start = Instant::now();
+    let start_timestamp = Instant::now();
 
+    print_sep();
+    println!("RKN-NETLIST NORMALIZER/AGGREGATOR V1.0");
     print_sep();
 
     let file = File::open(CSV_FILE).unwrap();
@@ -229,61 +217,55 @@ fn main() {
         ipv6_hosts: 0,
         ipv6_networks: 0,
     };
-    print_stat(&net_list, &original_stat);
-    let duration = start.elapsed();
-    println!("Duration:             {:#?}", duration);
-    print_sep();
+    print_stat(&net_list, &original_stat, start_timestamp);
 
+    let timestamp = Instant::now();
     net_list.sort();
     net_list.dedup();
     println!("after dedup");
-    print_stat(&net_list, &original_stat);
-    let duration = start.elapsed();
-    println!("Duration:             {:#?}", duration);
-    print_sep();
+    print_stat(&net_list, &original_stat, timestamp);
 
+    let timestamp = Instant::now();
     net_list = Ipv4Net::aggregate(&net_list);
     println!("after normalization");
-    print_stat(&net_list, &original_stat);
-    let duration = start.elapsed();
-    println!("Duration:             {:#?}", duration);
-    print_sep();
+    print_stat(&net_list, &original_stat, timestamp);
 
-//    let prefix = 30;
-    for prefix in (24..32).rev() {
-        if net_list.len() <= ROUTES_MAX {
-            break;
-        }
-
+    for prefix in (PREFIX..32).rev() {
         print_sep();
         println!("resizing with prefix = {:#?} ...", prefix);
+        let timestamp = Instant::now();
         net_list = net_list.resize_with_prefix(prefix);
-        print_stat(&net_list, &original_stat);
+        print_stat(&net_list, &original_stat, timestamp);
     }
 
-    let duration = start.elapsed();
     print_sep();
-    println!("Duration:             {:#?}", duration);
-    println!("Writing results to file... ");
+    println!("Duration: {:#?}", start_timestamp.elapsed());
 
     write_file(net_list);
 }
 
-fn print_stat(net_list: &Vec<Ipv4Net>, stats: &Stat) {
+fn parse_cmd_args() {
+    exit(1);
+}
+
+fn print_stat(net_list: &Vec<Ipv4Net>, stats: &Stat, start_time: Instant) {
     let s1 = net_list.len().to_string();
     let s1 = decimal_mark(s1);
     let s2 = net_list.size().to_string();
     let s2 = decimal_mark(s2);
 
     let size = mem::size_of::<Ipv4Net>();
-    let total = size * net_list.len();
+    let total = size * net_list.len() / 1024;
     let total = decimal_mark(total.to_string());
 
-    let nets_p: f32 = net_list.len() as f32 / stats.ipv4_networks as f32 * 100f32;
-    let hosts_p: f32 = net_list.size() as f32 / stats.ipv4_hosts as f32 * 100f32;
+    let nets_p: f32 = net_list.len() as f32 / stats.ipv4_networks as f32 * 100f32; // fixme div by 0
+    let hosts_p: f32 = net_list.size() as f32 / stats.ipv4_hosts as f32 * 100f32; // fixme div by 0
+
     println!("Nets:    {:>12}       {:>14.1}%", s1, nets_p);
     println!("Hosts:   {:>12}       {:>14.1}%", s2, hosts_p);
-    println!("Memory:  {:>12}       {:>15}", size, total);
+    println!("Memory:  {:>12}       {:>13} Kb", size, total);
+    println!("Duration:  {:#?}", start_time.elapsed());
+    print_sep();
 }
 
 fn decimal_mark(s: String) -> String {
@@ -299,4 +281,20 @@ fn decimal_mark(s: String) -> String {
     result
 }
 
-//        let _: () = self;
+fn write_file(ip_list: Vec<Ipv4Net>) {
+    let path = Path::new(OUT_FILE);
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .append(false)
+        .open(path)
+        .unwrap();
+
+    for net in ip_list {
+        let line = format!("route {:#?} reject;\n", net);
+        file.write_all(line.as_bytes())
+            .expect("Couldn't write to file");
+    }
+}
